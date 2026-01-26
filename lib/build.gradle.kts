83b7d10eb3f9f9131076ce15b27e99eb05ce5c72
Morgan Koh <delacrixmorgan@pm.me>
2026-01-26 20:29:12 +0100

    Update GitHub Actions

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,7 +1,53 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
-    id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.28.0"
+}
+
+mavenPublishing {
+    // Define coordinates for the published artifact
+    coordinates(
+        groupId = "io.github.delacrixmorgan",
+        artifactId = "spindler",
+        version = "0.1.0"
+    )
+
+    // Configure POM metadata for the published artifact
+    pom {
+        name.set("Spindler")
+        description.set("GEDCOM Kotlin Multiplatform Parser \uD83C\uDF33")
+        inceptionYear.set("2025")
+        url.set("https://github.com/delacrixmorgan/spindler")
+
+        licenses {
+            license {
+                name.set("GNU General Public License v3.0")
+                url.set("https://github.com/delacrixmorgan/spindler/blob/main/LICENSE.md")
+            }
+        }
+
+        // Specify developers information
+        developers {
+            developer {
+                id.set("delacrixmorgan")
+                name.set("Morgan Koh")
+                email.set("delacrixmorgan@gmail.com")
+            }
+        }
+
+        // Specify SCM information
+        scm {
+            url.set("https://github.com/delacrixmorgan/spindler")
+        }
+    }
+
+    // Configure publishing to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    // Enable GPG signing for all publications
+    signAllPublications()
 }
 
 kotlin {
